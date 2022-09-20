aa5b0c71e28a26fd17051cdba84c10965af0d422
tyassusanto <nasusayt@gmail.com>
2022-09-20 23:15:38 +0700

    add-context

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,18 +1,47 @@
-import React from 'react'
+import React, { useReducer } from 'react'
 import CartContext from './cart-context'
+
+const defaultCartState = {
+  items: [],
+  totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+  if(action.type === 'ADD'){
+    const updatedItems = state.items.concat(action.item)
+    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount
+    }
+  }
+  return defaultCartState
+}
 
 const CartProvider = (props) => {
 
-    const handleAddItemToCart = (item) => {}
+  const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState)
 
-    const handleRemoveItemFromCart = (id) => {}
+  const handleAddItemToCart = (item) => {
+    dispatchCartAction({
+      type:'ADD',
+      item: item
+    })
+  }
 
-    const cartContext = {
-        items: [],
-        totalAmount: 0,
-        addItem: handleAddItemToCart,
-        removeItem: handleRemoveItemFromCart
-    }
+  const handleRemoveItemFromCart = (id) => {
+    dispatchCartAction({
+      type: 'REMOVE',
+      id: id
+    })
+  }
+
+  const cartContext = {
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
+    addItem: handleAddItemToCart,
+    removeItem: handleRemoveItemFromCart
+  }
 
   return <CartContext.Provider value={cartContext}>
     {props.children}
