ce6b1ae5c6d5af7238d5d52ba789edb5ccd121a1
tyassusanto <nasusayt@gmail.com>
2022-09-20 14:27:52 +0700

    layout

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React, { Fragment } from 'react'
+import styles from './Header.module.css'
+import HeaderCartButton from './HeaderCartButton'
+
+const Header = (props) => {
+    return (
+        <Fragment>
+            <header className={styles.header}>
+                <h1>ReactMelas</h1>
+                <HeaderCartButton/>
+            </header>
+            <div className={styles['main-image']}>
+                <img src="https://raw.githubusercontent.com/academind/react-complete-guide-code/11-practice-food-order-app/extra-files/meals.jpg" alt="Pizza Limo" />
+            </div>
+        </Fragment>
+    )
+}
+
+export default Header
